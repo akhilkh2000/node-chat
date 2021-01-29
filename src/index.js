3d589163faf40e6e97efbe98e825b6dec00ab66a
akhil <akhilkhubchandani@gmail.com>
2021-01-29 20:05:37 +0530

    added socketio server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const express = require("express");
-const app = express();
 const path = require("path");
-
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const app = express();
+//refactoring done to create server in diff way to incorporate socket.io
+const server = http.createServer(app);
+const io = socketio(server); //create socketio server ( needs an http server as param)
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, (err) => {
+io.on("connection", () => {
+	console.log("New web socket connection");
+});
+
+server.listen(port, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
