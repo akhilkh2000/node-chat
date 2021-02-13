a0db93eb0ee3b97a0d6f0c9b23968832fd3319ae
akhil <akhilkhubchandani@gmail.com>
2021-02-13 13:10:38 +0530

    from and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,25 +4,38 @@ const socket = io(); //connect to socket
 //server(emit) -> client(recieve) -- ACK -> server
 // client(emit) -> server(recieve) -- ACK -> client
 
-document.getElementById("message-form").addEventListener("submit", (e) => {
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButtom = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+socket.on("message", (msg) => {
+	console.log(msg);
+});
+$messageForm.addEventListener("submit", (e) => {
 	e.preventDefault(); //to prevent default refresh
+	$messageFormButtom.setAttribute("disabled", "disabled"); //disable it for time until message is sent
 	const message = e.target.elements.message.value;
 	// console.log(message);
 	//sends a callback function so that the server can call in to ack the client
 	socket.emit("sendMessage", message, (msgFromserver) => {
+		//we reenable our button in our ACK callback
+		$messageFormButtom.removeAttribute("disabled");
+		// clear input
+		$messageFormInput.value = "";
+		$messageFormInput.focus(); //to re focus cursor on input
 		//event ACK
 		console.log("message from server - " + msgFromserver);
 	});
 });
-socket.on("message", (msg) => {
-	console.log(msg);
-});
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
 	if (!navigator.geolocation) {
 		//no support for this in the browser
 		return alert("Geolocation is not supported by your browser!");
 	}
+
+	//disable button while location is being fetched and sent
+	$locationButton.setAttribute("disabled", "disabled");
 
 	navigator.geolocation.getCurrentPosition((position) => {
 		console.log(position);
@@ -30,6 +43,8 @@ document.querySelector("#send-location").addEventListener("click", () => {
 		const long = position.coords.longitude;
 		const location = { lat, long };
 		socket.emit("sendLocation", location, (msgFromserver) => {
+			//re enable button once location sent
+			$locationButton.removeAttribute("disabled");
 			console.log("message from server -", msgFromserver);
 		});
 	});
